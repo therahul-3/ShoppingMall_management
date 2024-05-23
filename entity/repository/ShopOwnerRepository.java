d1e73006f5d060c38fadcbef8e6e967dec7d046d
rahul <130242614+therahul-3@users.noreply.github.com>
2024-05-23 14:23:34 +0530

    Delete entity/repository directory

--- entity/repository/ShopOwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.shoppingmall.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tnsif.shoppingmall.entity.ShopOwner;
-
-public interface ShopOwnerRepository extends JpaRepository<ShopOwner, Long>{
-
-}
